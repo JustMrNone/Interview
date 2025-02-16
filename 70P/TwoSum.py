ae67320e608dd205979fcd73279a120bb80d2555
Kamyar Bashar <kamyar.bashar@gmail.com>
2025-02-16 08:48:18 +0330

    TwoSum

---
+++ 70P/TwoSum.py
@@ -0,0 +1,32 @@
+"""
+slow 
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indexes = []
+        
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    indexes.extend([i, j])
+        return indexes
+        
+n * n = O(n ^ 2)
+
+
+"""
+from typing import List 
+
+#faster O(N)
+
+def two_sum(nums: List[int] = [2, 11, 7, 15], target: int = 9):
+    hash_map = {}
+    
+    for index, value in enumerate(nums):
+        diff = target - value
+        if diff in hash_map:
+            ans = [index, hash_map[diff]]
+            return ans 
+        
+        hash_map[value] = index
+        
+        
+print(two_sum())
